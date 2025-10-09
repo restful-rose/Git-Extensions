554d9d400e36184860b0f3e314d4c66f5f018014
Nicolai Krohg <nick@nve.no>
2025-10-09 15:46:51 +0200

    feat(Prun-OldBranches): add way to remove old branches autoamtically

---
+++ Prune-OldBranches.ps1
@@ -0,0 +1,52 @@
+﻿param (
+    [switch]$Force,
+    [switch]$DryRun,
+    [Parameter(Mandatory=$false)]
+    [int]$CutoffDays = 180
+)
+
+# Define cutoff date
+$cutoff = (Get-Date) - (New-TimeSpan -Days $CutoffDays)
+
+# Prune local cache of origin to sync with remote server
+git remote prune origin
+
+$remoteBranches = git branch -r --format="%(refname:short)" | ForEach-Object { ($_ -replace '^origin/') }
+
+if ($DryRun)
+{
+    Write-Host "Branches to be deleted:"
+}
+git for-each-ref --sort=committerdate --format="%(refname:short) %(committerdate:short)" refs/heads/ |
+ForEach-Object {
+    $parts  = $_ -split '\s+'
+    $branch = $parts[0]
+    $date   = Get-Date $parts[1]
+	
+    if ($date -lt $cutoff) {
+        $existsRemotely = $remoteBranches -contains $branch
+        if (-not $existsRemotely) {
+            if ($DryRun) {
+                Write-Host "$branch"
+            }
+            else {
+                Write-Host "Branch: $branch"
+                Write-Host "Last updated: $($date.ToString('yyy-MM-dd'))"
+                $ans = $true
+                if (-not $Force) {
+                    $ans = Read-Host "Do you want to delete (y/n)?"
+                }
+                if ($ans -match "y") {
+                    git branch -d $branch > $null
+                    $returnCode = $LASTEXITCODE
+                    if ($returnCode -eq 0) {
+                        Write-Host "Removed branch $branch successfully"
+                    }
+                    else {
+                        Write-Error "Failed to remove branch $branch"
+                    }
+                }
+            }
+        }
+    }
+}
